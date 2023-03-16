557440b35bc9fe59f8820de22a7aedc7ac4167fb
Jake Kerr <kodafox@gmail.com>
2023-03-16 12:54:07 +0900

    Add a debug system to keep an eye on KiraContext info while developing

--- examples/play_sound.rs
+++ examples/play_sound.rs
@@ -1,9 +1,11 @@
 use bevy::{prelude::App, DefaultPlugins};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_mod_kira::KiraPlugin;
 
 pub fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(KiraPlugin)
+        .add_plugin(WorldInspectorPlugin::new())
         .run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use std::fmt::Debug;
+use std::fmt::Formatter;
+use std::time::Duration;
+
+use bevy::prelude::info;
 use bevy::{
     app::Plugin,
     prelude::{
@@ -12,6 +17,7 @@ use kira::{
         backend::cpal::CpalBackend, error::PlaySoundError, AudioManager, AudioManagerSettings,
     },
     sound::static_sound::{StaticSoundData, StaticSoundHandle},
+    tween::Tween,
 };
 use static_sound_loader::{StaticSoundAsset, StaticSoundFileLoader};
 
@@ -81,23 +87,26 @@ impl Plugin for KiraPlugin {
             .add_asset::<StaticSoundAsset>()
             .add_asset_loader(StaticSoundFileLoader)
             .add_startup_system(setup_sys)
-            .add_system(play_sys);
+            .add_system(play_sys)
+            .add_system(tweak_mod_sys)
+            .add_system(debug_kira_sys);
     }
 }
 
-fn setup_sys(mut commands: Commands, loader: Res<AssetServer>) {
+fn setup_sys(mut commands: Commands, loader: Res<AssetServer>, mut time: ResMut<Time>) {
+    time.set_wrap_period(Duration::from_secs(10));
     let a = loader.load("sfx.ogg");
     commands.spawn(KiraSoundHandle(a));
 }
 
-struct Looper {
+struct TimerMs<const N: i32> {
     timer: Timer,
 }
 
-impl Default for Looper {
+impl<const N: i32> Default for TimerMs<N> {
     fn default() -> Self {
         Self {
-            timer: Timer::from_seconds(10.0, bevy::time::TimerMode::Repeating),
+            timer: Timer::from_seconds(N as f32 / 1000.0, bevy::time::TimerMode::Repeating),
         }
     }
 }
@@ -108,7 +117,7 @@ fn play_sys(
     assets: Res<Assets<StaticSoundAsset>>,
     query: Query<(Entity, &KiraSoundHandle)>,
     time: Res<Time>,
-    mut looper: Local<Looper>,
+    mut looper: Local<TimerMs<5000>>,
 ) {
     looper.timer.tick(time.delta());
     if !looper.timer.just_finished() {
@@ -124,4 +133,78 @@ fn play_sys(
             commands.entity(eid).insert(s);
         }
     }
+}
+
+fn tweak_mod_sys(
+    mut query: Query<&mut KiraSoundController>,
+    time: Res<Time>,
+    mut mod_wheel: Local<TimerMs<300>>,
+) {
+    mod_wheel.timer.tick(time.delta());
+    if !mod_wheel.timer.just_finished() {
+        return;
+    }
+    for mut controller in query.iter_mut() {
+        let _ = controller.0.set_panning(
+            (time.elapsed_seconds_wrapped_f64() * 3.0).sin(),
+            Tween::default(),
+        );
+    }
+}
+
+struct DebugKiraManager<'a> {
+    manager: &'a AudioManager,
+}
+
+struct DebugKiraContext<'a> {
+    manager: Option<DebugKiraManager<'a>>,
+}
+
+impl<'a> Debug for DebugKiraContext<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("KiraContext")
+            .field("manager", &self.manager)
+            .finish()
+    }
+}
+
+impl<'a> Debug for DebugKiraManager<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Manager")
+            .field("state", &self.manager.state())
+            .field("num_sounds", &self.manager.num_sounds())
+            .field("num_sub_tracks", &self.manager.num_sub_tracks())
+            .field("num_clocks", &self.manager.num_clocks())
+            .field("sound_capacity", &self.manager.sound_capacity())
+            .field("sub_track_capacity", &self.manager.sub_track_capacity())
+            .field("clock_capacity", &self.manager.clock_capacity())
+            .finish()
+    }
+}
+
+// Because KiraContext uses a sync cell we have to jump through some hoops to make a debug type that
+// can be printed via the debug trait which takes a non-mutable reference.
+impl<'a> From<&'a mut KiraContext> for DebugKiraContext<'a> {
+    fn from(context: &'a mut KiraContext) -> Self {
+        let manager_borrow = context.get_manager();
+        Self {
+            manager: match manager_borrow {
+                Some(manager) => Some(DebugKiraManager { manager }),
+                None => None,
+            },
+        }
+    }
+}
+
+fn debug_kira_sys(
+    mut kira: ResMut<KiraContext>,
+    time: Res<Time>,
+    mut looper: Local<TimerMs<1000>>,
+) {
+    looper.timer.tick(time.delta());
+    if !looper.timer.just_finished() {
+        return;
+    }
+    let context: DebugKiraContext = kira.as_mut().into();
+    info!("{:?}", context);
 }
